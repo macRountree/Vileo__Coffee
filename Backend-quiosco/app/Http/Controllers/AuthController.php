6fca6d8aada86fc74cb60cb3a59c1823dea0ea73
AngelLaines <angelmoreno755@gmail.com>
2023-10-04 14:31:27 -0700

    Registro de usuarios creado

---
+++ Backend-quiosco/app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistroRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(RegistroRequest $request){
+        $data = $request->validated();
+
+        // crear usuario
+
+        $user = User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password']),
+        ]);
+
+        // Retornar respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user'=> $user,
+        ];
+    }
+    public function login(Request $request){
+
+    }
+    public function logout(Request $request){
+
+    }
+}
