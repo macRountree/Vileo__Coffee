42efb966ae159ecf1adc37185f5bcfd9eabc97cf
AngelLaines <angelmoreno755@gmail.com>
2023-10-05 14:43:09 -0700

    Auth complete

--- Backend-quiosco/app/Http/Controllers/AuthController.php
+++ Backend-quiosco/app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -29,8 +30,29 @@ class AuthController extends Controller
     }
     public function login(LoginRequest $request){
         $data=$request->validated();
+
+        // Revisar el password
+
+        if (!Auth::attempt($data)) {
+            return response([
+                'errors'=>['El email o el password son incorrectos']
+            ],422);
+        }
+
+        // Autenticando
+
+        $user = Auth::user();
+        return [
+            'token'=>$user->createToken('token')->plainTextToken,
+            'user'=>$user,
+        ];
     }
     public function logout(Request $request){
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
+        return [
+            'user'=>null
+        ];
     }
 }
